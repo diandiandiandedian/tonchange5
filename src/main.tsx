d28855b2f78379b74edc6e2b66093a2b36b12050
zerooooooo1 <xushiduo1@gmail.com>
2024-06-22 16:59:37 +0800

    init

--- src/main.tsx
+++ src/main.tsx
@@ -8,10 +8,7 @@ import {TonConnectUIProvider} from "@tonconnect/ui-react"
 // const manifestUrl = 'https://gist.githubusercontent.com/siandreev/75f1a2ccf2f3b4e2771f6089aeb06d7f/raw/d4986344010ec7a2d1cc8a2a9baa57de37aaccb8/gistfile1.txt';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <TonConnectUIProvider manifestUrl="https://tonchange5.vercel.app/tonconnect-manifest.json"
-                        actionsConfiguration={{
-                            twaReturnUrl: 'https://t.me/zeroooooo1ferqw_bot/tonnew'
-                        }}>
+  <TonConnectUIProvider manifestUrl="https://tonchange5.vercel.app/tonconnect-manifest.json">
     <App />
   </TonConnectUIProvider>
 )
